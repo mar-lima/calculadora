58ef6e5bebf0be8a852b3f9de36a2ec42a11d4be
Marcelo Lima <marcelo.llima135@gmail.com>
2023-01-13 15:31:25 -0300

    meu primeiro commit

---
+++ JS/script.js
@@ -0,0 +1,25 @@
+
+function limps() {
+    document.getElementById('resp').value = "";
+};
+
+function insert(num) {
+    //escreve dentro do inpu => .value
+    var numero = document.getElementById('resp').value + ' ';
+    document.getElementById('resp').value = numero + num;
+};
+
+
+function calcular() {
+    var resp = document.getElementById('resp').value;
+    if(resp) {
+        document.getElementById('resp').value = eval(resp);
+    }
+    
+};
+
+
+
+
+
+
